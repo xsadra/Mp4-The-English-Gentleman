a6140b07b030999446198f16ba4707d843eb4c5a
Sadra <ti9mail@gmail.com>
2018-04-16 08:10:38 +0200

    Add spring structure

---
+++ src/main/java/de/sadrab/Mp4TheEnglishGentleman/Mp4TheEnglishGentlemanApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp4TheEnglishGentleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4TheEnglishGentlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4TheEnglishGentlemanApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sadrab/Mp4TheEnglishGentleman/Mp4TheEnglishGentlemanApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sadrab.Mp4TheEnglishGentleman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp4TheEnglishGentlemanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
